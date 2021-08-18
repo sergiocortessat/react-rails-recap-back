57906f8ed0fbf7f209d9820c74fc4a1097af317c
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-18 19:27:15 +0200

    Add logix changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_17_141705) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "sub"
     t.string "title"
